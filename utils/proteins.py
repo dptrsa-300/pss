b679bd8284f40eaf50dc75d81c72eb16ddd48682
skylerroh <skylerroh@gmail.com>
2021-09-29 03:01:52 +0000

    changes to structure parsing

--- utils/proteins.py
+++ utils/proteins.py
@@ -7,15 +7,35 @@ from pdbx.reader import PdbxReader
 from pdbx.reader import DataContainer
 
 
-def get_protein_sequence_from_cif(cif_contents):
+def parse_cif(cif_contents):
     data = []
     pRd = PdbxReader(io.StringIO(cif_contents))
     pRd.read(data)
     block = data[0]
+    return block
 
-    obj = block.get_object("struct_ref")
+
+def get_protein_sequence_from_cif(parsed_cif):
+    obj = parsed_cif.get_object("struct_ref")
     df = pd.DataFrame(obj._row_list, columns=obj._attribute_name_list)
     return df
+
+
+def get_atom_sites_from_cif(parsed_cif):
+    obj = parsed_cif.get_object("atom_site")
+    df = pd.DataFrame(obj._row_list, columns=obj._attribute_name_list)
+    return df
+
+
+def reduce_sequence_df(df):
+    return df.sort_values(['pdbx_db_accession', 'pdbx_align_begin'])\
+        .groupby(['pdbx_db_accession', 'db_code', 'db_name'])\
+        .agg({"pdbx_seq_one_letter_code": reduce_sequences, "protein_name": lambda x: x.iloc[0]})\
+        .reset_index()
+
+
+def reduce_sequences(seqs):
+    return ''.join(seqs.str.strip().replace('\n','', regex=True))
 
 
 def sequence_to_fasta_format(protein_name, sequence):
