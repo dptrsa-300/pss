47bbdf4acb768ad51e3f6803ea8a4b2d0da9ca64
skylerroh <skylerroh@gmail.com>
2021-10-07 14:55:13 +0000

    add confidence and sort atoms fix

--- utils/proteins.py
+++ utils/proteins.py
@@ -42,10 +42,35 @@ def get_atom_sites_from_cif(parsed_cif):
     return df
 
 
+def get_global_confidence_from_cif(parsed_cif):
+    obj = parsed_cif.get_object("ma_qa_metric_global")
+    return pd.DataFrame(obj._row_list, columns=obj._attribute_name_list).iloc[0]["metric_value"]
+
+
+def get_local_confidence_from_cif(parsed_cif):
+    # local confidence is indexed by the amino acid sequence
+    obj = parsed_cif.get_object("ma_qa_metric_local")
+    df = pd.DataFrame(obj._row_list, columns=obj._attribute_name_list)
+    return df
+
+
+def join_atoms_with_confidence(atoms, local_confidence):
+    min_amino_index = atoms.label_seq_id.astype(int).min() - 1
+    amino_index = atoms.label_seq_id.astype(int) - min_amino_index
+    
+    atoms_index = atoms.label_comp_id + amino_index.astype(str)
+    atoms_w_index = atoms.set_index(atoms_index)
+    
+    confidence_index = local_confidence.label_comp_id + local_confidence.label_seq_id
+    confidence_w_index = pd.DataFrame(local_confidence.metric_value.rename("confidence_pLDDT")).set_index(confidence_index)
+    
+    return atoms_w_index.join(confidence_w_index, how="left")
+
+
 def reduce_sequence_df(df):
     return df.sort_values(['pdbx_db_accession', 'pdbx_align_begin'])\
-        .groupby(['pdbx_db_accession', 'db_code', 'db_name'])\
-        .agg({"pdbx_seq_one_letter_code": reduce_sequences, "protein_name": lambda x: x.iloc[0]})\
+        .groupby(['pdbx_db_accession', 'db_code', 'db_name', "protein_id"])\
+        .agg({"pdbx_seq_one_letter_code": reduce_sequences, "protein_filename": lambda x: x.iloc[0]})\
         .reset_index()
 
 
@@ -57,9 +82,10 @@ def sort_by_file_number_and_index(atom_df):
     """
     This utility method is used to make sure that atoms are returned in order based on the file part that they came in and the id within that file
     """
-    atom_df["file_number"] = atom_df.protein_name.apply(protein_file_number)
-    sorted_atom_df = atom_df.sort_values(["file_number", "id"]).drop(columns=["file_number"])
-    return sorted_atom_df
+    atom_df["id"] = atom_df["id"].astype(int)
+    atom_df["file_number"] = atom_df.protein_filename.apply(protein_file_number)
+    sorted_atom_df = atom_df.sort_values(["protein_id", "file_number", "id"]).drop(columns=["file_number"])
+    return sorted_atom_df.reset_index(drop=True)
 
 
 def sequence_to_fasta_format(protein_name, sequence):
