2923f013a12fefcedc67f725526c1a705dafa07c
Linda <xlindadong@gmail.com>
2021-11-06 01:04:53 -0700

    added model stats, added cluster clickable bubble chart, added external uniprot links, fixed margins

--- viz/dash/app.py
+++ viz/dash/app.py
@@ -1,5 +1,8 @@
 import dash
+#import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__,
-    suppress_callback_exceptions=True)
+    suppress_callback_exceptions=True,
+    #external_stylesheets=[dbc.themes.QUARTZ]
+    )
 server = app.server
--- viz/dash/app1.py
+++ viz/dash/app1.py
@@ -5,6 +5,11 @@ from dash import callback_context
 from dash import Input, Output
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import webbrowser
+import pickle
+import numpy as np
+
 
 from app import app
 
@@ -29,6 +34,12 @@ else:
     table_df = pd.read_parquet(table_path)
     table_df = table_df.rename(columns={'target_protein': 'result_protein', 'query_protein': 'target_protein'})
 
+    model_path = "/Users/linda/Downloads/mvp_input_2021_1029_0054_model_overview.pkl"
+    with open(model_path, 'rb') as f:
+	    model_dict = pickle.load(f)
+    model_dict['Median number of proteins per cluster'] = cluster_df['count'].median()
+    model_dict['Total number of proteins'] = cluster_df['count'].sum()
+
 
 #cluster_df = cluster_df.sample(1000)
 #df = df.sample(1000)
@@ -38,7 +49,7 @@ df = df.astype({'Cluster Label': 'int32'})
 
 protein_indicators = df['protein'].unique()
 cluster_indicators = df['Cluster Label'].unique()
-table_columns = [c for c in ['result_protein', 'aligned_length', 'bitscore', 'evalue', 'rmsd', 'tmalign_score'] if c in table_df.columns]
+table_columns = [c for c in ['target_protein', 'result_protein', 'aligned_length', 'bitscore', 'evalue', 'rmsd', 'tmalign_score'] if c in table_df.columns]
 #table_columns = [c for c in ['target_protein', 'result_protein', 'aligned_length', 'rmsd', 'tmalign_score'] if c in table_df.columns]
 #table_columns = [c for c in ['aligned_length', 'rmsd', 'tmalign_score'] if c in table_df.columns]
 
@@ -53,8 +64,22 @@ last_views = None
 
 
 layout = html.Div([
-    html.H1('AlphaFold Protein Structural Similarity Explorer'),
-    html.H2('Query by Protein or Cluster'),
+    html.H1('AlphaFold2 Protein Structural Similarity Explorer'),
+	html.Div([
+        html.H6('Model Summary', style={'display': 'inline', 'margin-right': '10px'}),
+        html.Div(children= '%s: %s' % ('Total Proteins', model_dict['Total number of proteins']),
+            id='model-noise', className='modelstats'),
+		html.Div(children= '%s: %s' % ('Total Clusters', model_dict['Number of clusters (excl. noise)']),
+            id='model-clusters', className='modelstats'),
+        html.Div(children= '%s: %2.2f%%' % ('Percent of Proteins Unclustered' ,model_dict['Noise as % of total'] * 100),
+            id='model-noise-percent', className='modelstats'),
+        html.Div(children= '%s: %s Proteins' % ('Largest Cluster' ,model_dict['Largest non-noise cluster']),
+            id='model-non-noise', className='modelstats'),
+        html.Div(children= '%s: %d Proteins' % ('Median Cluster Size', model_dict['Median number of proteins per cluster']),
+            id='model-median', className='modelstats'),
+    ],
+	style={'width': '100%', 'display': 'inline-block', 'padding': '0px 0px 15px 0px', }),
+    #html.H5('Query by Protein or Cluster'),
     html.Div([
         html.Div([
             dcc.Dropdown(
@@ -77,14 +102,39 @@ layout = html.Div([
         ],
         style={'width': '50%', 'display': 'inline-block'}),
     ],
-    style={'width': '50%', 'display': 'inline-block'}),
+    style={'width': '50%', 'display': 'inline-block', 'padding': '0px 0px 0px 0px'}),
+
+
+        html.Div([
+                #html.H5('Cluster Comparisons'),
+			html.Div([
+				html.H6('Cluster Explorer'),
+            	dcc.Graph(
+                	id='cluster-num-hist',
+				)
+			],
+		    className='panel',
+        	style={'width': '48%', 'display': 'inline-block'}),
+		
+			html.Div([
+				html.H6('Protein Sequence Lengths Per Cluster'),
+            	dcc.Graph(
+                	id='cluster-protein-length',
+				)
+			],
+		    className='panel',
+        	style={'width': '48%', 'display': 'inline-block',
+               'float': 'right',}),
+		],
+		style={'width': '100%', 'display': 'inline-block', 'padding': '0px 0px 10px 0px'}),
+
 
     html.Div([
-        html.H2('Protein Comparisons'),
+        #html.H5('Protein Comparisons'),
         html.Div([
             html.Div([
             html.Div([
-                html.H3('Explorer'),
+                html.H6('Protein Explorer'),
             ],
 	    style={'width': '25%', 'display': 'inline-block', 
                    }),
@@ -94,29 +144,30 @@ layout = html.Div([
                     disabled=True),
             ],
 	    style={'width': '25%', 'display': 'inline-block', 
-                   'float': 'right', 'padding': '25px 0px'}),
+                   'float': 'right', 'padding': '12px 0px'}),
             html.Div([
                 html.Button('Functional View', n_clicks=0,
                     id='functional-view-button', className='view_button'),
             ],
 	    style={'width': '25%', 'display': 'inline-block', 
-                   'float': 'right', 'padding': '25px 0px'}),
+                   'float': 'right', 'padding': '12px 0px'}),
             html.Div([
                 html.Button('Confidence View', n_clicks=0,
                     id='confidence-view-button', className='view_button'),
             ],
 	    style={'width': '25%', 'display': 'inline-block', 
-                   'float': 'right', 'padding': '25px 0px'}),
+                   'float': 'right', 'padding': '12px 0px'}),
             ]),
             dcc.Graph(
                 id='cluster-3D-scatter',
                 #hoverData={'points': [{'customdata': 'Japan'}]}
-            )
+            ),
         ],
-        style={'width': '50%', 'display': 'inline-block'}),
+		className='panel',
+        style={'width': '48%', 'display': 'inline-block'}),
 
         html.Div([
-            html.H3('Results and Evaluation'),
+            html.H6('Protein Results and Evaluation'),
             dash_table.DataTable(
                 id='results-table',
                 columns=[{"name": i, "id": i} for i in table_columns],
@@ -128,31 +179,11 @@ layout = html.Div([
         		page_size=10,
             ),
         ],
-        style={'width': '50%', 'display': 'inline-block',
-               'float': 'right', 'padding': '0px 0px 0px 0px'}),
+		className='panel',
+        style={'width': '48%', 'display': 'inline-block',
+               'float': 'right',}),
     ],
-    style={'width': '100%', 'display': 'inline-block'}),
-
-        html.Div([
-                html.H2('Cluster Comparisons'),
-			html.Div([
-				html.H3('Number of Proteins Per Cluster'),
-            	dcc.Graph(
-                	id='cluster-num-hist',
-				)
-			],
-        	style={'width': '50%', 'display': 'inline-block'}),
-		
-			html.Div([
-				html.H3('Protein Sequence Lengths'),
-            	dcc.Graph(
-                	id='cluster-protein-length',
-				)
-			],
-        	style={'width': '50%', 'display': 'inline-block',
-               'float': 'right', 'padding': '0px 0px 0px 0px'}),
-		],
-		style={'width': '100%', 'display': 'inline-block'}),
+    style={'width': '100%', 'display': 'inline-block', 'padding': '0px 0px 10px 0px'}),
 
 ])
 
@@ -191,14 +222,28 @@ def toggle_view():
     Output('cluster-view-button', 'disabled'),
     Output('confidence-view-button', 'disabled'),
     Output('functional-view-button', 'disabled'),
+    Output('cluster_label_filter', 'value'),
+	Output('cluster-num-hist', 'clickData'),
     Input('protein_filter', 'value'),
     Input('cluster_label_filter', 'value'),
     Input('cluster-view-button', 'n_clicks'),
     Input('confidence-view-button', 'n_clicks'),
     Input('functional-view-button', 'n_clicks'),
+	Input('cluster-3D-scatter', 'clickData'),
+	Input('cluster-num-hist', 'clickData'),
     )
 def update_graph(protein, cluster_label, 
-            cluster_view_button, confidence_view_button, functional_view_button):
+            cluster_view_button, confidence_view_button, functional_view_button,
+            protein_click_data, cluster_click_data):
+    link = 'https://www.uniprot.org/uniprot/'
+    if protein_click_data:
+        link += protein_click_data['points'][0]['customdata'][0]
+        webbrowser.open(link)
+
+    if cluster_click_data:
+        cluster_label = [cluster_click_data['points'][0]['customdata']]
+        cluster_click_data = None
+
     # Handle View Buttons
     cluster_view, confidence_view, functional_view = toggle_view()
 
@@ -249,7 +294,8 @@ def update_graph(protein, cluster_label,
         color = scatter_dff['Y']
 
     scatter_fig = px.scatter_3d(scatter_dff, x=scatter_dff['X'], y=scatter_dff['Y'], z=scatter_dff['Z'],
-              color=color, hover_data=['protein', 'length'], color_discrete_map=color_discrete_map)
+              color=color, hover_data={'protein': True, 'length': True, 'Cluster Label': True}, color_discrete_map=color_discrete_map, labels={'color': 'Cluster', 'confidence': 'Confidence'})
+    scatter_fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), showlegend=True)
 
     """
     cluster_groups = dff.groupby(['Cluster Label'])
@@ -260,6 +306,7 @@ def update_graph(protein, cluster_label,
     points_count.reset_index(inplace=True)
     """
 
+    """
     cluster_dfff = cluster_dff[cluster_dff['count'] < 100]
     num_hist = px.histogram(
 	    x=cluster_dfff['count'],
@@ -267,7 +314,42 @@ def update_graph(protein, cluster_label,
         color_discrete_map=color_discrete_map,
 		#log_x=True,
 		nbins = 100,
+        labels ={'color': 'cluster'},
         )
+    """
+
+    cluster_groups = dff.groupby(['Cluster Label'])
+    points_count = cluster_groups.count()
+    points_count[['X', 'Y', 'Z']] = cluster_groups[['X', 'Y', 'Z']].mean()
+    points_count = points_count.reset_index()
+    df3 = points_count
+    df3['log_protein'] = df3['protein'].apply(lambda x: np.log(x))
+
+    """
+    num_hist = px.scatter_3d(df3, x=df3['X'], y=df3['Y'], z=df3['Z'], size=df3['log_protein'],
+              color=df3['Cluster Label'].astype(str), color_discrete_map=color_discrete_map)
+	"""
+
+    num_hist = go.Figure(data=go.Scatter3d(
+        x=df3['X'],
+        y=df3['Y'],
+        z=df3['Z'],
+		customdata=df3['Cluster Label'],
+        text=df3['Cluster Label'].astype(str),
+        mode='markers',
+        marker=dict(
+            sizemode='diameter',
+            sizeref=0.2,
+            size=df3['log_protein'],
+            color=df3['Cluster Label'].apply(lambda x: color_discrete_map[str(x)]),
+            #colorscale = 'sunsetdark',
+            #colorbar_title = 'Log(Number of Proteins)',
+            line_color='rgba(255, 255, 255, 0)'
+        )
+    ))
+    num_hist.update_layout(margin=dict(l=0, r=0, t=0, b=0), showlegend = False)
+
+
     """
 	num_hist = px.scatter_3d(points_count, 
 	          x=points_count['X'], y=points_count['Y'], z=points_count['Z'],
@@ -283,7 +365,11 @@ def update_graph(protein, cluster_label,
         color_discrete_map=color_discrete_map,
 		#log_x=True,
 		nbins = 100,
+        labels ={'color': 'Cluster'},
        )
+    length_hist.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+
+    # Model level data
 
     return [
             disable_protein_filter,
@@ -295,4 +381,6 @@ def update_graph(protein, cluster_label,
             cluster_view, 
             confidence_view,
 			functional_view,
+			cluster_label,
+			cluster_click_data,
            ]
